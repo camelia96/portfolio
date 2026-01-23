717c6792953f84464576b21f77e968a6123c682a
camelia <camelia.96str@gmail.com>
2026-01-23 13:05:58 +0100

    skill badge

---
+++ components/skill-badge.tsx
@@ -0,0 +1,7 @@
+export function SkillBadge() {
+    return (
+        <div className="bg-pink-100 text-pink-800 shadow-md rounded-full px-3.5 py-1 w-fit">
+            <p className="text-[15px] font-medium">Back-End</p>
+        </div>
+    )
+}
