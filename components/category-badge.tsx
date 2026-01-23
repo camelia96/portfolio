44aa54dc8b8ab62767d0dd71d842cfb193a4783e
camelia <camelia.96str@gmail.com>
2026-01-23 13:05:51 +0100

    category badge

---
+++ components/category-badge.tsx
@@ -0,0 +1,7 @@
+export function CategoryBadge() {
+    return (
+        <div className="bg-gray text-white shadow-md rounded-xl px-3.5 py-1 w-fit">
+            <p className="text-[15px] font-medium">React</p>
+        </div>
+    )
+}
