594e0f14f8fbf366c7742ce1458b9f8e616518c5
camelia <camelia.96str@gmail.com>
2026-01-26 11:25:26 +0100

    landing layout build

--- app/page.tsx
+++ app/page.tsx
@@ -1,106 +1,127 @@
-import { CategoryBadge } from "@/components/category-badge";
-import { WorkRecord } from "@/components/work-record";
-import Nav from "@/components/layout/Nav";
-import ProjectCard from "@/components/project-card";
-import { SkillBadge } from "@/components/skill-badge";
+import { CategoryBadge } from "@/app/ui/category-badge";
+import ProjectCard from "@/app/ui/project-card";
+import { SkillBadge } from "@/app/ui/skill-badge";
 import { prisma } from "@/lib/prisma";
-import { Copy } from "lucide-react";
 import Link from "next/link";
-import { AcademicRecord } from "@/components/academic-record";
-import { Footer } from "@/components/layout/Footer";
-import { SocialMedia } from "@/components/social-media";
+import { useProfileData } from "./hooks/useProfileData";
+import { projectInclude } from "@/types/types";
+import { AboutData } from "./ui/about";
+import { CvRecord } from "./ui/cv-record";
+import Nav from "./ui/layout/nav";
+import { Footer } from "./ui/layout/footer";
+import { supabase } from "@/lib/supabase";
+import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
+  // Projects
+  const projects = await prisma.projects.findMany({ include: projectInclude });
 
+  // SKills
+  const skills = await prisma.skills.findMany();
 
+  // Tech stack (Categories)
+  const categories = await prisma.categories.findMany();
 
-  // Data
-  const data = await prisma.data.findFirst();
-  if (!data) return;
+  // Curriculum
+  const curriculum = await prisma.cV.findMany();
+  if (!curriculum) return;
+
+  const academic = curriculum.filter((c) => c.academic)
+
+  const professional = curriculum.filter((c) => c.professional)
+
+  const { name, role, email, description, linkedin, github, behance, cv } = await useProfileData();
+
+  const social = { linkedin, github, behance }
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col max-w-10/12 m-10 font-sans text-black">
-        <Nav />
-        <div className="w-full flex flex-col justify-center gap-6 my-15">
+    <div className="flex flex-col items-center justify-center overflow-x-hidden">
+      <div className="flex flex-col max-w-10/12 m-10 font-sans text-black gap-20 lg:gap-26">
+        <div id="home" className="w-full flex flex-col justify-start gap-12">
+          <Nav />
+
           {/** NAME */}
+          <h1 className="text-center md:text-start uppercase">{name}</h1>
 
-          <h1 className="text-start mb-15 uppercase">{data.name}</h1>
+          {/** ABOUT */}
+          <AboutData about={{ role, description }} contact={{ email, social }} />
+        </div>
 
-          <div className="flex flex-col justify-between w-full gap-6" >
-            {/** ROLE */}
-            <div className="w-fit bg-[#FADFFF] px-5 py-2.5 rounded-full">
-              <p className="w-fit text-[28px] font-medium text-[#BE4BC4] tracking-[-0.3]">{data.role}</p>
+        <div className="flex flex-col gap-20">
+          <Separator />
+
+          {/** PROJECTS */}
+          <div id="projects">
+            <h2 className="leading-normal mb-6">projects.</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+              {projects &&
+                (<>
+                  {projects.map((p) => {
+
+                    const projectImg = p.files[0] ? supabase.storage.from("Files").getPublicUrl(p.files[0].file.path + p.files[0].file.name).data.publicUrl.toString() : null;
+
+                    return (<ProjectCard imgURL={projectImg} key={p.id} project={p} />)
+                  })}
+                </>)}
+            </div>
+          </div>
+
+
+          <Separator />
+
+          {/** CV */}
+          <div id="cv">
+            <div className="flex flex-col mb-12 ">
+              <h2>cv.</h2>
+              {/** DOWNLOAD CV */}
+              <Link href={cv} target="_blank" className="w-fit border-b border-black hover:text-gray hover:border-none transition duration-200">download <span className="font-bold">cv</span></Link>
             </div>
 
-            {/** DESCRIPTION */}
-            <p className="w-3/4">
-              {data.description}
-            </p>
+            <div className="flex flex-col gap-12">
 
-            {/** CONTACT DATA */}
+              {/** Tech stack */}
+              {skills &&
+                (<div>
+                  <h3 className="mb-2">Skills</h3>
+                  <div className="flex flex-wrap items-center gap-2">
+                    {skills.map((s) => (<SkillBadge key={s.id} skill={s} />))}
+                  </div>
+                </div>
+                )
+              }
+              {/** TECH STACK */}
+              {categories && (
+                <div>
+                  <h3 className="mb-2">Competencies</h3>
+                  <div className="flex flex-wrap items-center gap-2">
+                    {categories.map((c) => (<CategoryBadge key={c.id} category={c} />))}
+                  </div>
+                </div>
+              )}
 
-            <div className="flex gap-6 items-center">
-
-              <Link href={"#"} className="font-semibold text-[30px] tracking-sm underline capitalize">Lets get in touch.</Link>
-
-
-              <div className="flex gap-2 items-center">
-                <p className="font-mono tracking-sm ">{data.email}</p>
-                <Copy />
+              {/** PROFESSIONAL EXP */}
+              <div>
+                <h3 className="mb-2">Professional Experience</h3>
+                <div className="flex flex-wrap flex-col items-start gap-6">
+                  {professional.map((p) => (<CvRecord key={p.id} record={p} />))}
+                </div>
               </div>
 
 
-              <SocialMedia />
-
-
-
+              {/** ACADEMIC EXP */}
+              <div>
+                <h3 className="mb-2">Academic experience</h3>
+                <div className="flex flex-col items-start gap-6">
+                  {academic.map((a) => (<CvRecord key={a.id} record={a} />))}
+                </div>
+              </div>
             </div>
+
           </div>
         </div>
 
-        {/** PROJECTS */}
-        <h2 className="">projects.</h2>
-        <div className="grid grid-cols-3 gap-12">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-        </div>
-
-
-        {/** CV */}
-        <h2>cv.</h2>
-        <Link href={"#"} className="font-semibold text-[30px] tracking-sm underline">Download CV.</Link>
-
-        <div>
-          {/** SKILLS */}
-          <h3>Skills</h3>
-          <div className="flex items-center gap-6">
-            <SkillBadge />
-          </div>
-          {/** TECH STACK */}
-          <h3>Tech stack</h3>
-          <div className="flex items-center gap-6">
-            <CategoryBadge />
-          </div>
-
-          {/** PROFESSIONAL EXP */}
-          <h3>Professional Experience</h3>
-          <div className="flex items-center gap-6">
-            <WorkRecord />
-          </div>
-
-
-          {/** ACADEMIC EXP */}
-          <h3>Academic experience</h3>
-          <div className="flex items-center gap-6">
-            <AcademicRecord />
-          </div>
-        </div>
-
-        <Footer email={data?.email}/>
+        <Footer id="contact" contact={{ email, social }} />
       </div>
-
     </div>
   );
 }
