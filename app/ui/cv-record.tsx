567e3926283542d0068c8904e3cc92316821e609
camelia <camelia.96str@gmail.com>
2026-01-27 14:17:08 +0100

    cv record layout

--- app/ui/cv-record.tsx
+++ app/ui/cv-record.tsx
@@ -8,20 +8,23 @@ export function CvRecord({ record }: CvProps) {
 
     return (
         <div className="w-full flex flex-col  border-b-2 pb-4 border-gray-300 gap-4">
+            <div className="flex gap-2 items-center">
+                {record.icon_company && (<img src={record.icon_company} className="p-0.5 " width={50} height={50} alt="Logo" />)}
+                <div>
+                    <h4>{record.role}</h4>
+                    <p className="">{record.company}</p>
+                </div>
+
+            </div>
+
             <div>
-                <h4>{record.role}</h4>
-                <p className="text-gray font-medium">{record.timeline}</p>
+                <p className="text-gray font-semibold">{record.timeline}</p>
+                <p className="text-gray">{record.location}</p>
             </div>
 
             {bulletDescription ? (<ul className="list-disc">{bulletDescription.map((d, i) => (<li key={i}>{d}</li>))}</ul>) : ""}
 
-            <div>
-                <div>
-                    {record.icon_company && (<img src={record.icon_company} className="p-0.5 " width={100} height={100} alt="Logo" />)}
-                    <p className="">{record.company}</p>
-                </div>
-                <p className="text-gray">{record.location}</p>
-            </div>
+
         </div>
     )
 }
