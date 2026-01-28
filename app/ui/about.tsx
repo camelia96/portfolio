d012992003fccd6fa452f6ab2c8b1d18246b9657
camelia <camelia.96str@gmail.com>
2026-01-28 14:48:14 +0100

    responsive contact data

--- app/ui/about.tsx
+++ app/ui/about.tsx
@@ -24,7 +24,7 @@ export function AboutData({ about, contact }: AboutProps) {
 
             {/** CONTACT DATA */}
 
-            <div className="flex items-center gap-6 md:gap-8">
+            <div className="flex flex-col-reverse items-start md:flex-row md:items-center gap-6 md:gap-8">
 
                 <Link href={"#contact"} className="text-2xl font-semibold tracking-md border-b capitalize border-black hover:text-gray hover:border-none transition duration-200">Let's get in touch.</Link>
                 <div className="flex items-center">
